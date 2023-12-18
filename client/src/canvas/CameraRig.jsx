b1b370472e35a5a4c72a5029e129f893b8e50d4a
akshat-shah05 <akshatshah1605@gmail.com>
2023-12-18 18:19:00 -0500

    motion + damped easing added to shirt

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,8 +1,47 @@
+/* eslint-disable react/prop-types */
+import { useFrame } from "@react-three/fiber"
+import { useSnapshot } from "valtio"
+import { easing } from "maath"
+import state from "../store"
+import { useRef } from "react"
 
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state)
 
-const CameraRig = () => {
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // This is for the inital position
+    let targetPosition = [-0.4, 0, 2];
+
+    if (snap.intro) {
+        if (isBreakpoint) targetPosition=[0, 0, 2];
+        if (isMobile) targetPosition=[0, 0.2, 2.5];
+    } else {
+        if (isMobile) targetPosition=[0, 0, 2.5]
+        else targetPosition = [0, 0, 2]
+    }
+
+    // This is for setting the camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
+    // This is for making the model rotate with easing
+    easing.dampE(
+        group.current.rotation,
+        [state.pointer.y / 10, -state.pointer.x/5, 0],
+        0.25,
+        delta
+      )
+  })
+
+  
+
   return (
-    <div>CameraRig</div>
+    <group ref={group}>
+        {children}
+    </group>
   )
 }
 
