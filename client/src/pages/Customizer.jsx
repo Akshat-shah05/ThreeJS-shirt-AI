a6aedc751130d491e1945d56a5837c5fb12779c4
akshat-shah05 <akshatshah1605@gmail.com>
2024-01-25 23:57:10 -0500

    server fixes

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -44,17 +44,29 @@ const Customizer = () => {
   }
 
   const handleSubmit = async (type) => {
-    if(!prompt) {
-      return alert("Please enter a prompt")
-    }
+    if(!prompt) return alert("Please enter a prompt");
 
     try {
-      // Generate AI Image here
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
     } catch (error) {
-        alert(error)
+      alert(error)
     } finally {
-        setGeneratingImg(false)
-        setActiveEditorTab("")
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   }
 
